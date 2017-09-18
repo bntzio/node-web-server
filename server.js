089c70c145a365268981f1a388546e945dd55c85
Enrique Benitez <enriquebenitezjr@gmail.com>
2017-09-17 22:23:05 -0500

    Serve static content

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.send({
     name: 'Enrique Benitez',
@@ -19,4 +21,6 @@ app.get('/about', (req, res) => {
   res.send('<h1>About Page</h1>');
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is up on port 3000 ✨');
+});
