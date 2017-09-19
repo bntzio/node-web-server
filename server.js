b5a8d60cbc076d4ca2f7831705ea961feaaa7a56
Enrique Benitez <enriquebenitezjr@gmail.com>
2017-09-19 13:05:45 -0500

    Add handlebars partials and helpers

--- server.js
+++ server.js
@@ -3,21 +3,27 @@ const hbs = require('hbs');
 
 const app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Hi there! 👋',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Welcome to my website!'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
